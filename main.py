58c4a4047fa76bc94b95126dd415e45c589b2559
pramit1609 <153130665+pramit1609@users.noreply.github.com>
2023-12-08 13:06:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import os
 import json
 from sklearn.linear_model import LogisticRegression
@@ -7,8 +8,9 @@ from joblib import dump
 import sys
 import pandas as pd
 
-
+# Function to load data from CSV files in a specified directory
 def get_data(base_dir):
+     # List CSV files in the directory
     data_file_names = [x for x in os.listdir(base_dir) if x.endswith('.csv')]
     data = {}
     for name in data_file_names:
@@ -16,17 +18,17 @@ def get_data(base_dir):
         data[name] = pd.read_csv("C:\Users\pramit\PycharmProjects\pythonProject4\Model\Iris.csv")
     return data
 
-
+# Function to split data into training and testing sets
 def split_data(data, test_size=0.2, random_state=42):
     df = data['iris.csv']
     X = df.drop('variety', axis=1)
     y = df['variety']
-
+# Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
     return {'X_train': X_train, 'X_test': X_test, 'y_train': y_train, 'y_test': y_test}
 
-
+# Function to train a Logistic Regression model using grid search for hyperparameter tuning
 def train_model(X_train, y_train):
     clf = LogisticRegression(max_iter=1000, random_state=200)
     mod = GridSearchCV(clf, param_grid={'C': [0.001, 0.01, 0.1, 1, 10, 100]})
@@ -34,12 +36,12 @@ def train_model(X_train, y_train):
     m = mod.best_estimator_
     return m
 
-
+# Function to save the trained model to a file
 def save_model(m):
     dump(m, '../Model/irispred.joblib')
     print("Model saved")
 
-
+# Main script execution
 if __name__ == "__main__":
     base_dir = r'C:\Users\pramit\PycharmProjects\pythonProject3\Model\Iris.csv'
     data = get_data(base_dir)
